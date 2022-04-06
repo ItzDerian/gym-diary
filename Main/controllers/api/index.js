f78bd17c3d7c63bfceca3f4b45fd4022255fe758
Ben Lin <95650934+yogs0ddhoth@users.noreply.github.com>
2022-04-06 00:00:30 -0400

    projectroutes commented out to be reused

--- Main/controllers/api/index.js
+++ Main/controllers/api/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const projectRoutes = require('./projectRoutes');
+// const  = require('./ ');
 
 router.use('/users', userRoutes);
-router.use('/projects', projectRoutes);
+// router.use('/ ', );
 
 module.exports = router;
