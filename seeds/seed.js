5ae4799401dc1f4ec3587b0a75dae9a82b9f3dc1
Ben Lin <95650934+yogs0ddhoth@users.noreply.github.com>
2022-04-07 22:55:57 -0400

    to fix bug: exercise table now created before the log table

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,17 +1,10 @@
 const sequelize = require('../config/connection');
-<<<<<<< HEAD
-const { User, Project } = require('../models');
-
-const userData = require('./userData.json');
-const projectData = require('./projectData.json');
-=======
-const { User, FitnessGoal, Exercise, DailyLog } = require('../models');
+const { User, FitnessGoal, Exercise, Log } = require('../models');
 
 const userData = require('./userData.json');
 const fitnessGoalData = require('./fitnessGoalData.json');
 const exerciseData = require('./exerciseData.json');
 const dailyLogData = require('./dailyLogData.json');
->>>>>>> 8b4a49751bb06723078744cea764d875746c4f2b
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -20,33 +13,21 @@ const seedDatabase = async () => {
     individualHooks: true,
     returning: true,
   });
-
-<<<<<<< HEAD
-  for (const project of projectData) {
-    await Project.create({
-      ...project,
-      user_id: users[Math.floor(Math.random() * users.length)].id,
+  
+  for (const workout of exerciseData) {
+    await Exercise.create({
+      ...workout,
+      // user_id: users[Math.floor(Math.random() * users.length)].id,
     });
-  }
+  };
+  
+  for (const log of dailyLogData) {
+    await Log.create({
+      ...log,
+      // user_id: users[Math.floor(Math.random() * users.length)].id,
+    });
+  };
 
   process.exit(0);
-};
-
-=======
-    for (const log of dailyLogData) {
-      await DailyLog.create({
-        ...log,
-        user_id: users[Math.floor(Math.random() * users.length)].id,
-      });
-  
-};
-for (const workout of exerciseData) {
-  await Exercise.create({
-    ...workout,
-    user_id: users[Math.floor(Math.random() * users.length)].id,
-  });
-
-process.exit(0);
-}}
->>>>>>> 8b4a49751bb06723078744cea764d875746c4f2b
+}
 seedDatabase();
