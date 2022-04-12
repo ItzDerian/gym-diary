c635be235d24aba62f04c46a22813d397a131abb
Ben Lin <95650934+yogs0ddhoth@users.noreply.github.com>
2022-04-12 19:39:07 -0400

    moved get /log/:id to logRoutes import seqelize.fn

--- controllers/api/logRoutes.js
+++ controllers/api/logRoutes.js
@@ -1,41 +1,46 @@
 const router = require('express').Router();
 const { User, Log, Exercise } = require('../../models');
 const withAuth = require('../../utils/auth');
+const sequelize = require('../../config/connection');
 
 // user login routes: 'http://localhost:PORT/
-router.get('/log', withAuth, async (req, res) => {
+router.get('/:date', withAuth, async (req, res) => {
   try {
     console.log('test1');
-    // Find the logged in user based on the session ID
-   const userLog = await User.findByPk(req.sessions.user_id, {
-    attributes: { exclude: ['password'] },
-    include: [
-      {
-        model: Log,
-        attributes: [
-          'id',
-          [
-            sequelize.fn('DATE', sequelize.col('log_date')),
-            'log_date',
-          ], 
-          'sets', 
-          'reps'
-        ],
-        include: {
-          model: Exercise,
-          attributes: ['exercise', 'targetArea'],
-        }
+    // get single day
+    const logs = await Log.findAll({
+      attributes: [
+        'id',
+        'user_id',
+        'log_date', 
+        'sets', 
+        'reps'
+      ],
+      include: {
+        model: Exercise,
+        attributes: ['exercise', 'targetArea'],
       },
-    ],
-  })
-  console.log('test2');
-  // // Serialize data so the template can read it
-  const logs = userLog.map((log) => log.get({ plain: true }));
-  console.log(logs);
-  // render data in handlebars
-  res.render('logentry', { 
-    logs, 
-  });
+      where: [
+        sequelize.where(sequelize.fn('DATE', sequelize.col('log_date')), req.params.date), 
+        // for when session is set up
+        {user_id: req.session.user_id},
+
+        // // for testing:
+        // {user_id: req.params.id}
+      ]
+    });
+    console.log(logs)
+
+    // serialize the data
+    const dailyLog = logs.map(log => log.get({ plain: true }));
+
+    console.log(dailyLog);
+
+    // // render in handlebars
+    // res.render('logentry', {
+    //   ...dailyLog,
+    //   logged_in: req.session.logged_in
+    // });
   } catch (err) {
     res.status(500).json(err);
   }
